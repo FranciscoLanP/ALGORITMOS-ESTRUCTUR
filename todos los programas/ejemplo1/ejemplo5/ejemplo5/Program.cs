de53d54bac1ab9bcbd047c2df977550e2e098943
FranciscoLanP <168931664+FranciscoLanP@users.noreply.github.com>
2025-07-06 20:27:57 -0400

    Add files via upload

---
+++ todos los programas/ejemplo1/ejemplo5/ejemplo5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class EjemploHashSet
+{
+    public static void Main(string[] args)
+    {
+        HashSet<string> frutas = new HashSet<string>();
+
+        frutas.Add("Manzana");
+        frutas.Add("Banana");
+        frutas.Add("Naranja");
+        frutas.Add("Manzana"); 
+
+        Console.WriteLine("Frutas en el conjunto:");
+
+        foreach (string fruta in frutas)
+        {
+            Console.WriteLine(fruta);
+        }
+
+        if (frutas.Contains("Banana"))
+        {
+            Console.WriteLine("\nEl conjunto contiene Banana.");
+        }
+
+        if (frutas.Contains("Uva"))
+        {
+            Console.WriteLine("El conjunto contiene Uva.");
+        }
+        else
+        {
+            Console.WriteLine("El conjunto NO contiene Uva.");
+        }
+    }
+}
